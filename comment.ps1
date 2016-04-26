f58267f316ff78d3b31b83369f186321981d51c1
Andrei Linnik <linnik@mail.ru>
2016-04-26 19:18:34 +0300

    Repos-comp

---
+++ comment.ps1
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+      <title>Document</title>
+</head>
+<body>
+<<<<<<< HEAD
+<h1> учка вс€ческого прочего33333333333333333333333333333333333333333</h1>
+<<<<<<< HEAD
+<p>New Branch</p>
+=======
+<p>Nextgen --another bbb</p>
+>>>>>>> master
+=======
+<h1> учка всяческого прочего22 22 </h1>
+<p>Nextgen -- master REALLY nmaster</p>
+>>>>>>> master
+</body>
+</html>
